181d0e899fe37eb578ca0db5ebe7b53cdaadc92f
Daniel Friesel <derf@finalrewind.org>
2013-04-12 20:07:41 +0200

    more replays

--- src/ftdi.c
+++ src/ftdi.c
@@ -3,25 +3,39 @@
 #include <stdio.h>
 #include <usb.h>
 
+#define UCHAR(str) ( (unsigned char *) (str) )
+
+struct ftdi_context *ftdi;
+
+static void check_ret(char *desc, int ret)
+{
+	if (ret < 0)
+		fprintf(stderr, "%s: %d (%s)\n", desc, ret,
+				ftdi_get_error_string(ftdi));
+	else if (ret > 0)
+		fprintf(stdout, "%s ok: %d\n", desc, ret);
+}
+
+static void dump_buf(unsigned char *buf, int len)
+{
+	for (int i = 0; i < len; i++) {
+		fprintf(stdout, "%02x", buf[i]);
+	}
+	putc('\n', stdout);
+}
 
 int main(int count, char *argv[])
 {
 	unsigned char inbuf[64];
 	int ret;
 
-	struct ftdi_context *ftdi;
-
-	unsigned char echobuf = '\xaa';
-
 	if ((ftdi = ftdi_new()) == 0) {
 		puts("ftdi_new failed");
 		exit(1);
 	}
 
-	if (ftdi_usb_open(ftdi, 0x0403, 0xfac2) < 0) {
-		printf("error opening\n");
-		exit(1);
-	}
+	ret = ftdi_usb_open(ftdi, 0x0403, 0xfac2);
+	check_ret("open", ret);
 
 	printf("Typ: %d\n", ftdi->type);
 	printf("usb_read_timeout: %d\n", ftdi->usb_read_timeout);
@@ -31,20 +45,17 @@ int main(int count, char *argv[])
 	printf("bitbang_enabled: %x\n", ftdi->bitbang_enabled);
 	printf("bitbang_mode: %x\n", ftdi->bitbang_mode);
 
-	if (ftdi_set_latency_timer(ftdi, 0x0a) < 0) {
-		puts("set latency: ");
-		puts(ftdi_get_error_string(ftdi));
-	}
+	ret = ftdi_set_latency_timer(ftdi, 0x0a);
+	check_ret("latency", ret);
 
-	if (ftdi_set_baudrate(ftdi, 1000000) < 0) {
-		puts("set baudrate: ");
-		puts(ftdi_get_error_string(ftdi));
-	}
+	ret = ftdi_set_baudrate(ftdi, 1000000);
+	check_ret("baudrate", ret);
 
-	if ((ret = ftdi_set_bitmode(ftdi, 0, 2)) < 0) {
-		puts("set bitmode: ");
-		puts(ftdi_get_error_string(ftdi));
-	}
+	ret = ftdi_set_bitmode(ftdi, 0, 2);
+	check_ret("bitmode", ret);
+
+	ret = ftdi_usb_reset(ftdi);
+	check_ret("reset", ret);
 
 	printf("Typ: %d\n", ftdi->type);
 	printf("usb_read_timeout: %d\n", ftdi->usb_read_timeout);
@@ -54,27 +65,97 @@ int main(int count, char *argv[])
 	printf("bitbang_enabled: %x\n", ftdi->bitbang_enabled);
 	printf("bitbang_mode: %x\n", ftdi->bitbang_mode);
 
-	if ((ret = ftdi_write_data(ftdi, &echobuf, 1)) < 0) {
-		puts("write data: ");
-		puts(ftdi_get_error_string(ftdi));
+	ret = ftdi_write_data(ftdi, UCHAR("\xaa"), 1);
+	check_ret("write 0xaa", ret);
+
+	ret = ftdi_write_data(ftdi, UCHAR("\xaa"), 1);
+	check_ret("write 0xaa", ret);
+
+	ret = ftdi_read_data(ftdi, inbuf, 2);
+	check_ret("read", ret);
+	dump_buf(inbuf, 2);
+
+	ret = ftdi_write_data(ftdi, UCHAR("\xab"), 1);
+	check_ret("write 0xab", ret);
+
+	ret = ftdi_read_data(ftdi, inbuf, 4);
+	check_ret("read", ret);
+	dump_buf(inbuf, 4);
+
+	ret = ftdi_write_data(ftdi, UCHAR("\x81\x87"), 2);
+	check_ret("write", ret);
+
+	ret = ftdi_read_data(ftdi, inbuf, 1);
+	check_ret("read", ret);
+	dump_buf(inbuf, 1);
+
+	for (int i = 0; i < 26; i++) {
+		ret = ftdi_write_data(ftdi, UCHAR("\xaa\xaa\xaa"), 3);
+		check_ret("write", ret);
 	}
 
-	if ((ret = ftdi_write_data(ftdi, &echobuf, 1)) < 0) {
-		puts("write data: ");
-		puts(ftdi_get_error_string(ftdi));
-	}
+	ret = ftdi_read_data(ftdi, inbuf, 0x48);
+	check_ret("read", ret);
+	dump_buf(inbuf, 0x48);
 
-	if ((ret = ftdi_read_data(ftdi, inbuf, 2)) != 2) {
-		puts("read data: ");
-		puts(ftdi_get_error_string(ftdi));
-	}
+	ret = ftdi_write_data(ftdi, UCHAR("\xab"), 1);
+	check_ret("write 0xab", ret);
 
-	printf("< %02x%02x\n", inbuf[0], inbuf[1]);
+	ret = ftdi_read_data(ftdi, inbuf, 7);
+	check_ret("read", ret);
+	dump_buf(inbuf, 7);
 
-	if ((ret = ftdi_usb_close(ftdi)) < 0) {
-		fprintf(stderr, "usb_close: %d (%s)\n", ret,
-				ftdi_get_error_string(ftdi));
-	}
+	ret = ftdi_write_data(ftdi, UCHAR("\x00\x8f\x9f"), 3);
+	check_ret("write", ret);
+
+	ret = ftdi_set_latency_timer(ftdi, 0x01);
+	check_ret("latency", ret);
+
+	ret = ftdi_set_latency_timer(ftdi, 0x0a);
+	check_ret("latency", ret);
+
+	ret = ftdi_set_bitmode(ftdi, 0, 2);
+	check_ret("bitmode", ret);
+
+	ret = ftdi_usb_reset(ftdi);
+	check_ret("reset", ret);
+
+	printf("Typ: %d\n", ftdi->type);
+	printf("usb_read_timeout: %d\n", ftdi->usb_read_timeout);
+	printf("usb_write_timeout: %d\n", 
+		ftdi->usb_write_timeout);
+	printf("baudrate: %d\n",  ftdi->baudrate);
+	printf("bitbang_enabled: %x\n", ftdi->bitbang_enabled);
+	printf("bitbang_mode: %x\n", ftdi->bitbang_mode);
+
+	ret = ftdi_write_data(ftdi, UCHAR("\xaa"), 1);
+	check_ret("write 0xaa", ret);
+
+	ret = ftdi_write_data(ftdi, UCHAR("\xaa"), 1);
+	check_ret("write 0xaa", ret);
+
+	ret = ftdi_write_data(ftdi, UCHAR("\xaa"), 1);
+	check_ret("write 0xaa", ret);
+
+	ret = ftdi_write_data(ftdi, UCHAR("\xaa"), 1);
+	check_ret("write 0xaa", ret);
+
+	ret = ftdi_write_data(ftdi, UCHAR("\xaa"), 1);
+	check_ret("write 0xaa", ret);
+
+	ret = ftdi_read_data(ftdi, inbuf, 6);
+	check_ret("read", ret);
+	dump_buf(inbuf, 6);
+
+	ret = ftdi_write_data(ftdi, UCHAR("\xab"), 1);
+	check_ret("write 0xab", ret);
+
+	ret = ftdi_read_data(ftdi, inbuf, 6);
+	check_ret("read", ret);
+	dump_buf(inbuf, 6);
+
+	ret = ftdi_usb_close(ftdi);
+	check_ret("close", ret);
 
 	ftdi_free(ftdi);
 
